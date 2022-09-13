567008af9c106d15c128a60250f6df902cb8051a
Sreggie <darren.rogers06@gmail.com>
2022-09-13 18:37:24 -0400

    trying to fix mangerQuestions

--- develop/index.js
+++ develop/index.js
@@ -3,78 +3,109 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const Intern = require('./lib/Intern');
+let team = []
 // TODO: CODE GOES HERE
 const firstQuest = {
   type: 'list',
   message: 'What role would you like to add',
-  choices: ['Manager', 'Engineer', 'Intern'],
+  choices: ['Manager', 'Engineer', 'Intern', 'No more Roles'],
   name: 'whatRole',
 }
 const managerQuest = [
   {
-  type:'input',
-  message:'What is the managers name?',
-  name:'mngrName',
-},{
-  type:'input',
-  message:'What is the managers email?',
-  name:'mngrEmail',
-}
-,{
-  type:'input',
-  message:'What is the managers ID?',
-  name:'mngrId',
-}
-,{
-  type:'input',
-  message:'What is the managers office number?',
-  name:'mngrOffice',
-}
+    type: 'input',
+    message: 'What is the managers name?',
+    name: 'mngrName',
+  }, {
+    type: 'input',
+    message: 'What is the managers email?',
+    name: 'mngrEmail',
+  }
+  , {
+    type: 'input',
+    message: 'What is the managers ID?',
+    name: 'mngrId',
+  }
+  , {
+    type: 'input',
+    message: 'What is the managers office number?',
+    name: 'mngrOffice',
+  }
 ]
 
-const engineerQuest = [  {
+const engineerQuest = [{
   type: 'input',
-  message:'What is the engineers name?',
-  name:'engName',
-},{
+  message: 'What is the engineers name?',
+  name: 'engName',
+}, {
   type: 'input',
-  message:'What is the engineers email?',
-  name:'engEmail',
+  message: 'What is the engineers email?',
+  name: 'engEmail',
 }
-,{
+  , {
   type: 'input',
-  message:'What is the engineers ID?',
-  name:'engId',
+  message: 'What is the engineers ID?',
+  name: 'engId',
 }
-,{
+  , {
   type: 'input',
-  message:'What is the eningeers ID?',
-  name:'engGit',
+  message: 'What is the eningeers ID?',
+  name: 'engGit',
 }
 ]
-const internQuest = [ {
+const internQuest = [{
   type: 'input',
-  message:'What is the interns name?',
-  name:'inName',
-},{
+  message: 'What is the interns name?',
+  name: 'inName',
+}, {
   type: 'input',
-  message:'What is the interns email?',
-  name:'inEmail',
+  message: 'What is the interns email?',
+  name: 'inEmail',
 }
-,{
+  , {
   type: 'input',
-  message:'What is the interns ID?',
-  name:'ingId',
+  message: 'What is the interns ID?',
+  name: 'ingId',
 }
-,{
+  , {
   type: 'input',
-  message:'What is the interns school?',
-  name:'inSchol',
+  message: 'What is the interns school?',
+  name: 'inSchol',
 }]
 
+
+const managerQuestions = function (answers) {
+  inquirer.prompt(managerQuest).then((answers) => {
+    const manager = new Manager(answers.mngrName, answers.mngrId, answers.mngrEmail, answers.mngrOffice)
+    console.log(manager)
+    team.push(manager)
+    whatRole()
+  })
+}
+const engineerQuestions = function (answers) {
+  inquirer.prompt(engineerQuest)
+    .then(function () {
+      const engineer = new Engineer(answers.engName, answers.engId, answers.engEmail, answers.engGit)
+      console.log(engineer)
+      team.push(engineer)
+      whatRole()
+    })
+}
+const internQuestions = function (answers) {
+  inquirer.prompt(internQuest)
+    .then(function () {
+      const Intern = new Intern(answers.inName, answers.inId, answers.inEmail, answers.inGit)
+      console.log(Intern)
+      team.push(Intern)
+      whatRole()
+    })
+}
+const returnHome = function () {
+  fs.writeFileSync(``)
+}
 const whatRole = function () {
-  inquirer.prompt(firstQuest).then(function (answers) {
+  inquirer.prompt(firstQuest).then((answers) => {
     if (answers.whatRole === 'Manager') {
       managerQuestions()
     }
@@ -83,22 +114,9 @@ const whatRole = function () {
     }
     if (answers.whatRole === 'Intern') {
       internQuestions()
-    }else{
+    } else {
       returnHome()
     }
   })
-}
-const managerQuestions = function (answers) {
-  inquirer.prompt(managerQuest)
-  .then(function(){
-    const manager = new Manager( answers.mngrName,answers.mngrId, answers.mngrEmail, answers.mngrOfficde)
-    console.log(manager)
-  })
-}
-const engineerQuestions = function (answers) {
-
-}
-const internQuestions = function (answers) {
-
 }
 whatRole()
