1bc4d4d57f31c8ef031fbf6570b85da09c4df0c1
Sreggie <darren.rogers06@gmail.com>
2022-09-13 16:01:58 -0400

    made class questions

--- develop/index.js
+++ develop/index.js
@@ -11,12 +11,67 @@ const firstQuest = {
   choices: ['Manager', 'Engineer', 'Intern'],
   name: 'whatRole',
 }
-const managerQuest = [{
+const managerQuest = [
+  {
+  type:'input',
+  message:'What is the managers name?',
+  name:'mngrName',
+},{
+  type:'input',
+  message:'What is the managers email?',
+  name:'mngrEmail',
+}
+,{
+  type:'input',
+  message:'What is the managers ID?',
+  name:'mngrId',
+}
+,{
+  type:'input',
+  message:'What is the managers office number?',
+  name:'mngrOffice',
+}
+]
 
+const engineerQuest = [  {
+  type: 'input',
+  message:'What is the engineers name?',
+  name:'engName',
+},{
+  type: 'input',
+  message:'What is the engineers email?',
+  name:'engEmail',
+}
+,{
+  type: 'input',
+  message:'What is the engineers ID?',
+  name:'engId',
+}
+,{
+  type: 'input',
+  message:'What is the eningeers ID?',
+  name:'engGit',
+}
+]
+const internQuest = [ {
+  type: 'input',
+  message:'What is the interns name?',
+  name:'inName',
+},{
+  type: 'input',
+  message:'What is the interns email?',
+  name:'inEmail',
+}
+,{
+  type: 'input',
+  message:'What is the interns ID?',
+  name:'ingId',
+}
+,{
+  type: 'input',
+  message:'What is the interns school?',
+  name:'inSchol',
 }]
-
-const engineerQuest = [{}]
-const internQuest = [{}]
 
 const whatRole = function () {
   inquirer.prompt(firstQuest).then(function (answers) {
@@ -28,6 +83,22 @@ const whatRole = function () {
     }
     if (answers.whatRole === 'Intern') {
       internQuestions()
+    }else{
+      returnHome()
     }
   })
 }
+const managerQuestions = function (answers) {
+  inquirer.prompt(managerQuest)
+  .then(function(){
+    const manager = new Manager( answers.mngrName,answers.mngrId, answers.mngrEmail, answers.mngrOfficde)
+    console.log(manager)
+  })
+}
+const engineerQuestions = function (answers) {
+
+}
+const internQuestions = function (answers) {
+
+}
+whatRole()
